0183e6837cf136a986ca9b9116d1e2591eab951d
ericapbs <pereiraerica.b@gmail.com>
2021-08-24 13:57:18 -0300

    inclusao de pastas exercicios

---
+++ Lógica/Program.cs
@@ -0,0 +1,40 @@
+﻿/*Desafio
+Você terá desafio de escrever um programa que leia dois valores X e Y. A seguir, mostre uma sequência de 1 até Y, passando para a próxima linha a cada X números.
+
+Entrada
+O arquivo de entrada contém dois valores inteiros, (1 < X < 20) e (X < Y < 100000).
+
+Saída
+Cada sequência deve ser impressa em uma linha apenas, com 1 espaço em branco entre cada número, conforme exemplo abaixo. Não deve haver espaço em branco após o último valor da linha.*/
+
+using System;
+
+namespace Dio.Logica
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] vet = Console.ReadLine().Split(' ') ;
+            int x,y,count;
+            x = int.Parse(vet[0]);
+            y = int.Parse(vet[1]);
+            count = 1;
+
+            for (int i = 1;i <= y;i++)
+            {
+            Console.Write(i);
+            if (x == count)
+            {
+                Console.WriteLine();
+                count = 1;
+            }
+              else if(count < x)
+            {
+                Console.Write(" ");
+                count++;
+        }
+    }
+}
+    }
+    }
---
+++ Maior/Program.cs
@@ -0,0 +1,37 @@
+﻿/*Desafio
+Desenvolva um programa capaz de ler três valores e apresentar o maior deles e adicionar a mensagem “ eh o maior”. Use a seguinte forma como base:
+
+Obs.: a fórmula apenas calcula o maior entre os dois primeiros (a e b). Um segundo passo, portanto é necessário para chegar no resultado esperado.
+
+Entrada
+O arquivo de entrada contém três valores inteiros.
+
+Saída
+Imprima o maior dos três valores seguido pela mensagem " eh o maior".*/
+
+using System;
+
+namespace Dio.Maior
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int A, B, C, MAIORAB, MAIOR;
+            
+            string[] valores = Console.ReadLine().Split(' ');
+            
+            A = int.Parse(valores[0]);
+            B = int.Parse(valores[1]);
+            C = int.Parse(valores[2]);
+
+            MAIORAB = (A + B + Math.Abs(A - B))/ 2;
+
+            MAIOR = (MAIORAB + C + Math.Abs (MAIORAB - C))/ 2;
+            
+            Console.WriteLine(MAIOR + " eh o maior");
+            //Console.WriteLine($"{MAIOR} eh o maior");
+            Console.ReadLine();
+        }
+    }
+}
